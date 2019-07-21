7c4b15f6581aef8e9d6b6525285571683a493272
Guillermo Vaya <guivaya@gmail.com>
2019-07-22 00:19:58 +0200

    do response, wrap errors

--- client.go
+++ client.go
@@ -1,16 +1,20 @@
 package gocrapi
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"time"
-	"errors"
+
+	"github.com/pkg/errors"
 )
 
 // Client holds info about how to connect to clash's api
 type Client struct {
-	token string
+	token   string
 	timeout time.Duration
-	baseUrl string
+	baseURL string
 }
 
 const officialAPI = "https://api.royaleapi.com"
@@ -21,32 +25,33 @@ func New(token string) (*Client, error) {
 		return nil, errors.New("Token not provided")
 	}
 	c := Client{
-		token: token,
+		token:   token,
 		timeout: 10 * time.Second,
-		baseUrl: officialAPI,
+		baseURL: officialAPI,
 	}
 	return &c, nil
 }
 
-func (Client *c) newClient() http.Client {
+func (c *Client) newClient() http.Client {
 	return http.Client{Timeout: c.timeout}
 }
 
-func (Client *c) Get(item string, params url.Values) ([]bytes, error) (
-	request, err := http.NewRequest("GET", c.baseUrl+item, nil)
+func (c *Client) Get(item string, params url.Values) ([]byte, error) {
+	request, err := http.NewRequest("GET", c.baseURL+item, nil)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "Error creating request")
 	}
 	request.Header.Add(
-		"Authorization", 
-		fmt.Sprintf("Bearer %s", c.token)
+		"Authorization",
+		fmt.Sprintf("Bearer %s", c.token),
 	)
-	request.URL.RawQuery = params.Encode
+	request.URL.RawQuery = params.Encode()
 	hc := c.newClient()
 	response, reqErr := hc.Do(request)
 	if reqErr != nil {
-		return nil, err
+		return nil, errors.Wrap(reqErr, "Error doing request")
 	}
+	// TODO: maybe this should just return a response object and handle it elsewhere
 	defer response.Body.Close()
-	return response.Body, nil
-)
+	return ioutil.ReadAll(response.Body)
+}
